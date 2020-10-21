054894a8c505d31d39d1c42b63d635dbdc821e8e
Michał <michal.tecza97@gmail.com>
2020-10-21 16:53:58 +0200

    lastName field added

--- Main.java
+++ Main.java
@@ -10,12 +10,17 @@ class PeopleStats {
 
 class Person {
     private final String firstName;
+    private final String lastName;
 
-    public Person(String firstName) {
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
     }
 
     public String getFirstName() {
         return firstName;
     }
+    public String getlastName() {
+        return lastName;
+    }
+
 }
